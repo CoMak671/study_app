a6945c36a138b0657931bd496fb8f16822405ee9
liuqiaoyi <liuqiaoyi@meitunmama.com>
2022-03-14 15:05:30 +0800

    test

---
+++ android/app/src/main/kotlin/com/example/mono_repo_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mono_repo_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mono_repo_demo_package_one/lib/mono_repo_demo_package_one.dart
@@ -0,0 +1,7 @@
+library mono_repo_demo_package_one;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
